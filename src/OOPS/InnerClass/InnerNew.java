ca6e597e9fabc7d47d5b5b38e6744788cddafbf4
Nikhil <nikhilmagadum68128@gmail.com>
2026-05-21 16:29:37 +0530

    lamda expression

--- src/OOPS/InnerClass/InnerNew.java
+++ src/OOPS/InnerClass/InnerNew.java
@@ -1,11 +1,19 @@
 package OOPS.InnerClass;
 
 @FunctionalInterface                 // Functional Interface which has only one method
-interface convertor {
+interface Convertor {
     void display();
 }
 
-class ABC implements convertor {
+interface Add {
+    int add(int a,int b);
+}
+
+
+interface Cube {
+    int findCube(int x);
+}
+class ABC implements Convertor {
     @Override
     public void display() {
         System.out.println("Hello functional interface ");
@@ -19,14 +27,23 @@ class Main {
         void display() {
             System.out.println("This is an inner class:");
         }
-    static void main(String[] args) {
-        ABC ab =  new ABC();
-        ab.display();
 
-        InnerNew in = new InnerNew();
-      in.display();
+        static void main(String[] args) {
+//            Convertor c = () -> {
+//                System.out.println("This is an anonymous class or lamda expression ");
+//            };
+//            c.display();
+
+           Add sum  = (int a ,int b ) -> {  return a+b;  };
+            int res = sum.add(5,10);
+            System.out.println(res);
+
+            Cube cc = (int x) -> {  return  x*x*x; };
+
+              int cube_res = cc.findCube(3);
+            System.out.println(cube_res);
+
         }
-
 
     }
 
