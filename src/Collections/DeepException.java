0e1da7db69829582d76389c8df14c30e0214654a
Nikhil <nikhilmagadum68129@gmail.com>
2026-05-28 01:14:06 +0530

    Exception Hirarchy

---
+++ src/Collections/DeepException.java
@@ -0,0 +1,21 @@
+package Collections;
+
+import java.util.Scanner;
+
+public class DeepException {
+    static void main(String[] args) {
+    try {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a / b;
+        System.out.println(c);
+    }
+    catch(Throwable e) {
+        System.out.println("Catch Block");
+        System.out.println(e.getMessage());
+        System.out.println(e);
+        e.printStackTrace();
+    }
+    }
+}
