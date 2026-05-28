f811457781d061f88a182a2aa5b8a53065914568
Nikhil <nikhilmagadum68129@gmail.com>
2026-05-28 21:47:47 +0530

    Custome Exception Example

---
+++ src/Collections/RBI.java
@@ -0,0 +1,56 @@
+package Collections;
+import java.util.Scanner;
+
+class Atm {
+    int pin = 1234;
+    int p;
+
+    public void acceptInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Pin");
+        p = sc.nextInt();
+    }
+
+    public void verify() throws Exception {
+        if (pin == p) {
+            System.out.println("Collect Money");
+        } else {
+            System.out.println("Invalid Input");
+            InvalidInputException ie = new InvalidInputException();
+            throw ie;
+        }
+    }
+}
+
+class InvalidInputException extends Exception {
+
+}
+class Bank {
+
+   public void Init() {
+       Atm a = new Atm();
+    try {
+        a.acceptInput();
+        a.verify();
+    } catch (Exception e) {
+           try{
+               a.acceptInput();
+               a.verify();
+           }catch(Exception d) {
+              try {
+                  a.acceptInput();
+                  a.verify();
+              }catch(Exception f) {
+                  System.out.println("Card Blocked.Please Contact bank");
+              }
+           }
+    }
+    }
+}
+
+public class RBI {
+    static void main(String[] args) {
+        Bank b =  new Bank();
+        b.Init();
+    }
+}
