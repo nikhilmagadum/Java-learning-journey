584883df4d54edb4f6b5992d2c4403c07a213804
Nikhil <nikhilmagadum68129@gmail.com>
2026-06-01 23:03:01 +0530

    logic test

--- src/PracticeFile/Multoo.java
+++ src/PracticeFile/Multoo.java
@@ -1,7 +1,15 @@
 package PracticeFile;
+import java.util.Scanner;
 
 public class Multoo {
     static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of multiplication table : ");
 
+        int n =  sc.nextInt();
+
+        for (int i = 1 ; i <= 10; i++) {
+            System.out.println(n + " X " + i + " = " + (n * i));
+        }
     }
 }
