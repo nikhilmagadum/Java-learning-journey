e906b196993fa3e35ab233533c4d3557cec13ae5
Nikhil <nikhilmagadum68129@gmail.com>
2026-06-09 18:47:20 +0530

    Boxing and Auto Boxing

---
+++ src/Collections_Framework/Box.java
@@ -0,0 +1,16 @@
+package Collections_Framework;
+import java.util.ArrayList;
+public class Box {
+    static void main(String[] args) {
+
+        int a = 10;
+
+        Integer i = new Integer(a);
+        Integer auto = a;
+        System.out.println(i);
+        System.out.println(a);
+        System.out.println(auto);
+
+        //Boxing is the process of converting a primitive type into its corresponding wrapper object.
+    }
+}
